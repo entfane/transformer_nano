ad6179e48cdf655cc57ab057dc2a11f9f46b8087
entfane <nik77kez@gmail.com>
2024-12-07 21:12:07 +0100

    Added dropouts and fixed little bugs

--- bigram_model.py
+++ bigram_model.py
@@ -5,14 +5,17 @@ from torch.nn import functional as F
 
 #hyperparams
 
-batch_size = 32
-block_size = 8
+batch_size = 64
+block_size = 256
 max_iters = 5000
-eval_interval = 300
-learning_rate = 1e-3
+eval_interval = 500
+learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embed = 32
+n_embed = 384
+n_head = 6
+n_layer = 6
+dropout = 0.2
 
 
 torch.manual_seed(1337)
@@ -37,12 +40,6 @@ val_data = data[n:]
 
 block_size = 8
 train_data[:block_size+1]
-
-x = train_data[:block_size]
-y = train_data[1:block_size+1]
-
-batch_size = 4
-block_size = 8
 
 def get_batch(split):
     data = train_data if split == 'train' else val_data
@@ -75,16 +72,16 @@ class Head(nn.Module):
     self.query = nn.Linear(n_embed, head_size, bias=False)
     self.value = nn.Linear(n_embed, head_size, bias=False)
     self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+    self.dropout = nn.Dropout(dropout)
 
   def forward(self, x):
     B, T, C = x.shape
     k = self.key(x)
     q = self.query(x)
     wei = q @ k.transpose(-2, -1) * C**-0.5
-
-    tril = torch.tril(torch.ones(T, T))
     wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
     wei = F.softmax(wei, dim=-1)
+    wei = self.dropout(wei)
     v = self.value(x)
     out = wei @ v
     return out
@@ -93,20 +90,22 @@ class MultiHeadAttention(nn.Module):
   def __init__(self, num_heads, head_size):
     super().__init__()
     self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
-    self.proj = nn.Linear(n_embed, n_embed)
+    self.proj = nn.Linear(head_size * num_heads, n_embed)
+    self.dropout = nn.Dropout(dropout)
 
   def forward(self, x):
     out = torch.cat([h(x) for h in self.heads], dim=-1)
-    out = self.proj(out)
+    out = self.dropout(self.proj(out))
     return out
 
 class FeedForward(nn.Module):
   def __init__(self, n_embed):
     super().__init__()
     self.net = nn.Sequential(
-      nn.Linear(n_embed, n_embed),
+      nn.Linear(n_embed, 4 * n_embed),
       nn.ReLU(),
-      nn.Linear(n_embed, n_embed),
+      nn.Linear(4 * n_embed, n_embed),
+      nn.Dropout(dropout),
     )
   
   def forward(self, x):
@@ -126,18 +125,14 @@ class Block(nn.Module):
     x = x + self.ffwd(self.ln2(x))
     return x
 
-class BigramLanguageModel(nn.Module):
+class GPT(nn.Module):
 
   def __init__(self):
     super().__init__()
     self.token_embedding_table = nn.Embedding(vocab_size, n_embed)
     self.position_embedding_table = nn.Embedding(block_size, n_embed)
-    self.blocks = nn.Sequential(
-      Block(n_embed, n_head = 4),
-      Block(n_embed, n_head = 4),
-      Block(n_embed, n_head=4),
-      nn.LayerNorm(n_embed)
-    )
+    self.blocks = nn.Sequential(*[Block(n_embed, n_head = n_head) for _ in range(n_layer)])
+    self.ln_f = nn.LayerNorm(n_embed)
     self.lm_head = nn.Linear(n_embed, vocab_size)
 
   def forward(self, idx, targets=None):
@@ -147,6 +142,7 @@ class BigramLanguageModel(nn.Module):
     pos_emb = self.position_embedding_table(torch.arange(T, device=device))
     x = token_emb + pos_emb
     x = self.blocks(x)
+    x = self.ln_f(x)
     logits = self.lm_head(x)
     # print(logits[0][0])
     if targets is None:
@@ -171,7 +167,7 @@ class BigramLanguageModel(nn.Module):
 
     return idx
 
-model = BigramLanguageModel()
+model = GPT()
 m = model.to(device)
 
 optimizer = torch.optim.AdamW(m.parameters(), lr = learning_rate)
